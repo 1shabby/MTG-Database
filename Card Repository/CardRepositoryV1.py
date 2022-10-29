651316b6b14135b8300bb87d14ce5163509f69d5
1shabby <67873454+1shabby@users.noreply.github.com>
2022-10-28 20:18:04 -0500

    First Push
    Contains the deprecated version (1.0) and the current version (2.0). Need to update comments on version 1.0, however version 2.0 is the most up to date algorithm.

---
+++ Card Repository/CardRepositoryV1.py
@@ -0,0 +1,207 @@
+#manages Google Sheets API
+from distutils.debug import DEBUG
+from re import M
+from urllib.parse import _NetlocResultMixinBase
+import gspread
+#Manages Generic HTTP requests
+import requests
+#Manages Sleep
+from time import sleep
+
+#Global Debug Flag
+Debug = True
+class UpdateSheet:
+    Calls = 0
+    FailedRequests = []
+    def UpdateLegendPrices(self,sheet):
+        worksheet = sheet.worksheet("Legends")
+        self.Run(worksheet)
+
+    def UpdatePlaneswalkerPrices(self,sheet):
+        worksheet = sheet.worksheet("Planeswalkers")
+        self.Run(worksheet)
+
+
+    def UpdateLandPrices(self,sheet):
+        worksheet = sheet.worksheet("Lands")
+        self.Run(worksheet)
+
+    def UpdateGreenPrices(self,sheet):
+        worksheet = sheet.worksheet("Green Spells")
+        self.Run(worksheet)
+
+    def UpdateBluePrices(self,sheet):
+        worksheet = sheet.worksheet("Blue Spells")
+        self.Run(worksheet)
+    def UpdateWhitePrices(self,sheet):
+        worksheet = sheet.worksheet("White Spells")
+        self.Run(worksheet)
+    
+    def UpdateRedPrices(self,sheet):
+        worksheet = sheet.worksheet("Red Spells")
+        self.Run(worksheet)
+
+    def UpdateBlackPrices(self,sheet):
+        worksheet = sheet.worksheet("Black Spells")
+        self.Run(worksheet)
+
+    def UpdateColorlessPrices(self,sheet):
+        worksheet = sheet.worksheet("Colorless Spells")
+        self.Run(worksheet)
+
+    def UpdateBulkPrices(self,sheet):
+        worksheet = sheet.worksheet("Bulk")
+        self.Run(worksheet)
+    # No input
+    # Authorize the script to edit the sheet, and open the sheet
+    # Return none
+    def OpenSheet(self):
+        #Authorization
+        serviceAccount = gspread.service_account(filename="mtg-database-364703-802c3d8f67f4.json")
+        #Open the spreadsheet
+        sheet = serviceAccount.open("MTG Library")
+        return sheet
+    # Take in the scryfall API request url
+    # Grab the dictionary response from that API
+    # Return the dictionary response
+    def SendRequest(self,url,row):
+        retval = ""
+        response = requests.get(url)
+        retval = response.json()
+        #If reponse status is 404
+        if retval.get('status') == 404:
+            print("THE URL: " + str(url) + " HAS FAILED! CHECK SPELLING!")
+            broken = [url,row]
+            print(broken)
+            self.FailedRequests.append(broken)
+            retval = "bad url"
+        return retval
+    # Takes in a dictionary, foil and etched
+    # Grab the price associated either foil, etched, or non-foil
+    # Return the price of the card  
+    def GetPrice(self,dict,foil):
+        price = ""
+        if dict != "bad url":
+            if(foil == 'etched'):
+                price = dict.get('data', {})[0].get('prices').get('usd_etched')
+            elif(foil == 'foil'):
+                price = dict.get('data', {})[0].get('prices').get('usd_foil')
+            else:
+                price = dict.get('data', {})[0].get('prices').get('usd')
+            if Debug:
+                print("Price: " + price)
+        return price
+
+    # Takes in the Name and set of a card
+    # Generates the url used to send the scryfall API request
+    # Returns the generated url as a string
+    def GenerateUrl(self,name,set,number=""):
+        if str(number) != "None":
+            url = "https://api.scryfall.com/cards/search?pretty=true&q=in:paper+set:" + set + "+" + name + "+cn:" + number
+        else:
+            url = "https://api.scryfall.com/cards/search?pretty=true&q=in:paper+set:" + set + "+" + name
+        if Debug:
+            print("Generated URL for '" + name + " " + set + "': " + url) 
+        return url
+
+    def IsFoil(self,row,worksheet):
+        retval = ""
+        foil = str(worksheet.acell('C'+ str(row)).value)
+        self.ManageGoogleCalls()
+        etched = str(worksheet.acell('E'+ str(row)).value)
+        self.ManageGoogleCalls()
+        if foil == "TRUE":
+            retval = "foil"
+        elif etched == "TRUE":
+            retval = "etched"
+        else:
+            retval = "none"
+        return retval
+
+    def ManageGoogleCalls(self):
+        if self.Calls == 60:
+            print("REACHED 60 CALLS! Waiting 60 SECONDS!")
+            sleep(40)
+            self.Calls = 1
+        else:
+            self.Calls += 1
+        if Debug:
+                print("Google Calls: " + str(self.Calls))
+
+    def GetRow(self):
+        row = input("please enter the row to grab")
+        return row
+
+    def SetPreviousPrice(self,worksheet,row):
+       price =  worksheet.acell('I' + str(row)).value
+       self.ManageGoogleCalls()
+       worksheet.update('K'+str(row),price)
+       self.ManageGoogleCalls()
+
+    def UpdateCell(self,price, row, worksheet):
+        if price != "":
+            self.ManageGoogleCalls()
+            cell = 'I' + str(row)
+            worksheet.update(cell,float(price))
+
+    def RowNum(self,worksheet):
+        count = int(worksheet.acell('O1').value)
+        self.ManageGoogleCalls()
+        return count
+
+    def Run(self,worksheet):
+        rows = self.RowNum(worksheet)
+        row = 69
+        while  row < rows :
+           
+            name = 'A' + str(row)
+            set = 'H' + str(row)
+            number = 'G' + str(row)
+
+            name = str(worksheet.acell(name).value)
+            self.ManageGoogleCalls()
+            set = str(worksheet.acell(set).value)
+            self.ManageGoogleCalls()
+            number = str(worksheet.acell(number).value)
+            self.ManageGoogleCalls()
+
+            url = self.GenerateUrl(name,set,number)
+            dict = self.SendRequest(url,row)
+            foil = self.IsFoil(row,worksheet)
+            price = self.GetPrice(dict,foil)
+            self.SetPreviousPrice(worksheet,row)
+            self.UpdateCell(price,row,worksheet)
+            # Mandatory 50 millisecond gap between API requests as per Scryfall's request
+            sleep(0.05)
+            row += 1
+        self.PrintFailed( worksheet)
+
+    def PrintFailed(self,worksheet):
+        print("Failed URLS:")
+        for i in range(0,len(self.FailedRequests)) :
+            print("Url: " + self.FailedRequests[i] + " Row: " + str(self.FailedRequests[i][i]))
+        response = input("Once the rows were fixed, press enter...")
+        for l in range(0,len(self.FailedRequests)):
+            # Rerun each of the rows that were messed up
+            name = 'A' + str(self.FailedRequests[l][l])
+            set = 'H' + str(self.FailedRequests[l][l])
+            number = 'G' + str(self.FailedRequests[l][l])
+
+            name = str(worksheet.acell(name).value)
+            self.ManageGoogleCalls()
+            set = str(worksheet.acell(set).value)
+            self.ManageGoogleCalls()
+            number = str(worksheet.acell(number).value)
+            self.ManageGoogleCalls()
+            url = self.GenerateUrl(name,set,number)
+            self.SendRequest(url,self.FailedRequests[l][l])
+
+
+RunUpdate = UpdateSheet()   
+RunUpdate.Calls = 0
+sheet = RunUpdate.OpenSheet() 
+# RunUpdate.UpdatePlaneswalkerPrices(sheet)
+# RunUpdate.UpdateLegendPrices(sheet)
+# RunUpdate.UpdateGreenPrices(sheet)
+RunUpdate.UpdateBluePrices(sheet)
+#RunUpdate.UpdateLandPrices(sheet)
---
+++ Card Repository/CardRepositoryV2.py
@@ -0,0 +1,216 @@
+# Manages the Google Sheets side of the script
+import gspread
+# Manages sending Generic HTTP requests
+import requests
+# Manages Sleep for pausing between API calls for Scryfall
+from time import sleep
+# Manages Pandas for easy processing of the sheet data
+import pandas as pd
+# Authentication File Name
+import AuthFile
+#Global Debug Flag
+Debug = True
+
+# Manages all the functionality for updating the sheet, 
+# from grabbing the sheet data, to pulling the Scryfall API data,
+# To processing the prices, and pushing it back to the sheet; It manages it all.
+class UpdateSheet:
+
+    # Manages
+    Data = ''
+    # Stores the row count 
+    Rows = 0
+    # Stores a list of the failed requests
+    FailedRequests = []
+
+    # Method used to update all the Legends prices
+    def UpdateLegendPrices(self,sheet):
+        worksheet = sheet.worksheet("Legends")
+        self.Run(worksheet)
+
+    # Method used to update all the Planeswalker prices
+    def UpdatePlaneswalkerPrices(self,sheet):
+        worksheet = sheet.worksheet("Planeswalkers")
+        self.Run(worksheet)
+
+    # Method used to update all the Land prices
+    def UpdateLandPrices(self,sheet):
+        worksheet = sheet.worksheet("Lands")
+        self.Run(worksheet)
+
+    # Method used to update all the Green Card prices
+    def UpdateGreenPrices(self,sheet):
+        worksheet = sheet.worksheet("Green Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the Blue Card prices
+    def UpdateBluePrices(self,sheet):
+        worksheet = sheet.worksheet("Blue Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the White Card prices
+    def UpdateWhitePrices(self,sheet):
+        worksheet = sheet.worksheet("White Spells")
+        self.Run(worksheet)
+    
+    # Method used to update all the Black Card prices
+    def UpdateBlackPrices(self,sheet):
+        worksheet = sheet.worksheet("Black Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the Black Card prices
+    def UpdateRedPrices(self,sheet):
+        worksheet = sheet.worksheet("Red Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the Multicolor Card Prices  
+    def UpdateMulticolor(self,sheet):
+        worksheet = sheet.worksheet("Multicolor Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the Colorless Card prices
+    def UpdateColorlessPrices(self,sheet):
+        worksheet = sheet.worksheet("Colorless Spells")
+        self.Run(worksheet)
+
+    # Method used to update all the Bulk Card prices
+    def UpdateBulkPrices(self,sheet):
+        worksheet = sheet.worksheet("Bulk")
+        self.Run(worksheet)
+
+    # No input
+    # Authorize the script to edit the sheet, and open the sheet
+    # Return the sheet
+    def OpenSheet(self):
+        #Authorization
+        serviceAccount = gspread.service_account(filename=AuthFile.filename)
+        #Open the spreadsheet
+        sheet = serviceAccount.open("MTG Library")
+        return sheet
+
+    # Takes in the input of the worksheet
+    # Uses the Gspread API to grab all the records and uses Pandas to store it in a dataframe
+    # No returned value; Sets self.Data to the dataframe 
+    def GetWorksheetData(self,worksheet):
+        self.Data = pd.DataFrame(worksheet.get_all_records())
+
+    # No Input
+    # Gets the length of the dataframe stored in self.Data
+    # No Return; Sets self.Rows to be the length of self.Data
+    def GetRows(self):
+        self.Rows = len(self.Data.index)
+
+    # Take in the a Worksheet
+    # For each item in a given worksheet complete all the steps needed to update the data
+    # No Return; Last step sends the udpated self.data to the worksheet
+    def Run(self,worksheet):
+        self.GetWorksheetData(worksheet)
+        self.GetRows()
+        print(self.Rows)
+        row = 0
+        # Loop through the rows on the sheet
+        while int(row) < int(self.Rows):
+            # Generate the url used for the api request
+            url = self.GenerateUrl(row)
+            # Get the data from the scryfall api as a dictionary
+            dict = self.SendRequest(url,row)
+            # Get the price for the card from the dictionary
+            price = self.GetPrice(row,dict)
+            # Sets the previous price col to be the current price
+            self.SetPreviousPrice(row)
+            # Updates the price col to contain the price obtained from the dict
+            self.UpdatePrice(row,price)
+            # Mandatory 50 - 100 milliesecond wait between scryfall API calls
+            sleep(0.1)
+            row += 1
+        # Update the worksheet with the updated data in self.data
+        self.UpdateSheetData(worksheet)
+
+    # Takes in the Name and set of a card
+    # Generates the url used to send the scryfall API request
+    # Returns the generated url as a string
+    def GenerateUrl(self,row):
+        if str(self.Data.loc[row,"Collector Number"]) != "":
+            url = "https://api.scryfall.com/cards/search?pretty=true&q=in:paper+set:" + self.Data.loc[row,"Set"] + "+" + self.Data.loc[row,"Name"] + "+cn:" + str(self.Data.loc[row,"Collector Number"])
+        else:
+            url = "https://api.scryfall.com/cards/search?pretty=true&q=in:paper+set:" + self.Data.loc[row,"Set"] + "+" + self.Data.loc[row,"Name"]
+        print("Generated URL for '" + self.Data.loc[row,"Name"] + " | " + self.Data.loc[row,"Set"] + "' " + url) 
+        return url    
+
+    # Take in the scryfall API request url
+    # Grab the dictionary response from that API
+    # Return the dictionary response
+    def SendRequest(self,url,row):
+        retval = ""
+        response = requests.get(url)
+        retval = response.json()
+        # If reponse status is 404
+        if retval.get('status') == 404:
+            print("THE URL: " + str(url) + " HAS FAILED! CHECK SPELLING!")
+            self.FailedRequests.append([row])
+            print([row])
+            retval = "bad url"
+        return retval
+        
+    # Takes in a dictionary, foil and etched
+    # Grab the price associated either foil, etched, or non-foil
+    # Return the price of the card  
+    def GetPrice(self,row,dict):
+        price = ""
+        if dict != "bad url":
+            try:
+                if(self.Data.loc[row,"Extra"] == 'FOIL'):
+                    price = dict.get('data', {})[0].get('prices').get('usd_foil')
+                    if Debug:
+                        print("Price: " + price)
+                
+                elif(self.Data.loc[row,"Extra"] == 'ETCHED'):
+                    price = dict.get('data', {})[0].get('prices').get('usd_etched')
+                    if Debug:
+                        print("Price: " + price)
+                
+                else:
+                    price = dict.get('data', {})[0].get('prices').get('usd')
+                    if Debug:
+                        print("Price: " + price)
+            # If for whatever reason, we are unable to get a price given the input print an error and set price to "" in order to not send bad data    
+            except:
+                print("FAILED TO GET PRICE FOR " + str(self.Data.loc[row,"Name"]).upper())
+                price = ""
+        return price
+
+    # Takes in the current row
+    # Grabs the value of the current price and sets that to the old price before it gets updated
+    # No return; Updates the old price col 
+    def SetPreviousPrice(self,row):
+        self.Data.loc[row,"Old Price"] = self.Data.loc[row,"Price"]
+
+    # Takes in the current row and the price for that row
+    # Updates the price on the given row with the price given
+    # No return; 
+    def UpdatePrice(self,row,price):
+        self.Data.loc[row,"Price"] = price
+        # Set the price * quantity col to be the google sheet function used to calculate this value
+        self.Data.loc[row,"Price * Quantity"] = '=multiply(B' +str(row +2) + ',G' +str(row + 2) + ')'
+
+    # Takes in the worksheet
+    # Uses the Gspread API to update the whole worksheet in one step
+    # No return value
+    def UpdateSheetData(self,worksheet):
+        worksheet.update([self.Data.columns.values.tolist()] + self.Data.values.tolist(),value_input_option="USER_ENTERED")
+
+
+RunUpdate = UpdateSheet()   
+RunUpdate.Calls = 0
+sheet = RunUpdate.OpenSheet() 
+RunUpdate.UpdatePlaneswalkerPrices(sheet)
+RunUpdate.UpdateLegendPrices(sheet)
+RunUpdate.UpdateGreenPrices(sheet)
+RunUpdate.UpdateBluePrices(sheet)
+RunUpdate.UpdateWhitePrices(sheet)
+RunUpdate.UpdateLandPrices(sheet)
+RunUpdate.UpdateBulkPrices(sheet)
+RunUpdate.UpdateRedPrices(sheet)
+RunUpdate.UpdateColorlessPrices(sheet)
+RunUpdate.UpdateBlackPrices(sheet)
+RunUpdate.UpdateMulticolor(sheet)
