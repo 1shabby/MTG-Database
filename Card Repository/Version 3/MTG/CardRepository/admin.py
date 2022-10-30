fabd0cb362ece611a93e0c872eb742d019f01a6e
1shabby <67873454+1shabby@users.noreply.github.com>
2022-10-29 23:16:32 -0500

    Created the Django Models
    Created the Django Models which created the respective tables.

--- Card Repository/Version 3/MTG/CardRepository/admin.py
+++ Card Repository/Version 3/MTG/CardRepository/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Legend,Planeswalker,GreenSpells,BlueSpells,BlackSpells,WhiteSpells,RedSpells,ColorlessSpells,MulticolorSpells
 
 # Register your models here.
+admin.site.register(Legend)
+admin.site.register(Planeswalker) 
+admin.site.register(GreenSpells) 
+admin.site.register(BlueSpells) 
+admin.site.register(BlackSpells) 
+admin.site.register(WhiteSpells) 
+admin.site.register(RedSpells) 
+admin.site.register(ColorlessSpells) 
+admin.site.register(MulticolorSpells)  
--- Card Repository/Version 3/MTG/CardRepository/models.py
+++ Card Repository/Version 3/MTG/CardRepository/models.py
@@ -1,3 +1,113 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Legend(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class Planeswalker(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class Land(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class GreenSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class BlueSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class BlackSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class WhiteSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class RedSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class ColorlessSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class MulticolorSpells(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
+
+class Bulk(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=1)
+    extra = models.CharField(max_length=6,default="NONE")
+    set = models.CharField(max_length=5)
+    collector_number = models.IntegerField()
+    old_price = models.DecimalField(decimal_places=2,max_digits=6)
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    price_quantity = models.DecimalField(decimal_places=2,max_digits=6)
