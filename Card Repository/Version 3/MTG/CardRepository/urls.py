0c87aff7d358b05abc727c19c43502ce7f64830f
1shabby <67873454+1shabby@users.noreply.github.com>
2022-10-30 12:29:21 -0500

    Removed the API additions from the files
    Decided to get more experience with Django before adding a react front end on top of it.

--- Card Repository/Version 3/MTG/CardRepository/urls.py
+++ Card Repository/Version 3/MTG/CardRepository/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'legends', views.LegendView, 'legend')
 
 urlpatterns = [
     path('', views.index, name='index'),
